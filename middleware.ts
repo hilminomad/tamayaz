90e24b3f7af06acb08ddd38141c6aba24b28c9f5
hilminomad <hilmiansari@gmail.com>
2024-09-27 00:11:10 +0100

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes:  ["/", "/blog", "/api/webhook", "/api/uploadthing", "/api/uploads"]
+  publicRoutes:  ["/", "/blog" , "/api/webhook", "/api/uploadthing", "/api/uploads","/api/webhooks/clerk", "/faq", "/terms-and-conditions", "/api/upload"], // Add routes that should be public
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)"]
 });
 
 export const config = {
