15096e8193023f70b72d41d2c8da5000c831219e
hilminomad <hilmiansari@gmail.com>
2024-09-28 08:46:07 +0100

    check CORS

--- next.config.js
+++ next.config.js
@@ -6,3 +6,18 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/api/upload',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // Adjust for specific domains if needed
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
+    ];
+  },
+};
